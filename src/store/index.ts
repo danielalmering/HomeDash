8f916f36e3b6cab5a28be20a100e477c63505702
Lorenzo Vuur <epicokapi@gmail.com>
2017-10-17 15:37:49 +0200

    Fixed some things

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import localization from './localization';
-import { LocalizationState } from './localization';
+import modals from './modals';
 
 Vue.use(Vuex);
 
@@ -17,7 +17,8 @@ const store = new Vuex.Store<RootState>({
     mutations: {
     },
     modules: {
-        localization: localization
+        localization: localization,
+        modals: modals
     }
 });
 
