b2d7625c3dda93290458c5d81ff3d592254bbefc
Lorenzo Vuur <epicokapi@gmail.com>
2017-10-19 18:03:13 +0200

    Added functionality to sidebar

--- src/components/pages/profile/sidebar/sidebar.ts
+++ src/components/pages/profile/sidebar/sidebar.ts
@@ -1,12 +1,87 @@
-import { Component, Prop } from 'vue-property-decorator';
+import { Component, Watch } from 'vue-property-decorator';
 import Vue from 'vue';
 
 import './sidebar.scss';
 
+interface Performer {};
+
+type SidebarCategory = 'recommended' | 'peek' | 'favourites';
 
 @Component({
     template: require('./sidebar.tpl.html'),
 })
 export default class Sidebar extends Vue {
 
+    performers: Performer = [];
+    category: SidebarCategory = 'recommended';
+
+    query: any = {
+        limit: 20,
+        offset: 0,
+        performer: 0,
+        search: ''
+    }
+
+    mounted(){
+        this.query.performer = this.$route.params.id;
+
+        this.loadRecommended();
+    }
+
+    search(){
+        if(this.query.search === ''){
+            return;
+        }
+
+        this.loadPerformers();
+    }
+
+    setCategory(category: SidebarCategory){
+        if(this.category === category){
+            return;
+        }
+
+        this.category = category;
+
+        this.loadPerformers();
+    }
+
+    loadPerformers(){
+        if(this.category === 'recommended'){
+            this.loadRecommended();
+        } else if(this.category === 'favourites'){
+            this.loadFavorites();
+        } else {
+            this.loadPeek();
+        }
+    }
+
+    async loadRecommended(){
+        const performerResults = await fetch(`https://www.thuis.nl/api/performer/performer_accounts/recommended?limit=20&offset=0&performer=${this.query.performer}&search=${this.query.search}`, {
+            credentials: 'include'
+        });
+        const data = await performerResults.json();
+
+        this.performers = data.performerAccounts;
+    }
+
+    async loadFavorites(){
+        const userId = this.$store.state.authentication.user.id;
+
+        const performerResults = await fetch(`https://www.thuis.nl/api/client/client_accounts/${userId}/favorite_performers?limit=20&offset=0&performer=${this.query.performer}&search=${this.query.search}`, {
+            credentials: 'include'
+        });
+        const data = await performerResults.json();
+
+        this.performers = data.performerAccounts;
+    }
+
+    async loadPeek(){
+        const performerResults = await fetch(`https://www.thuis.nl/api/performer/performer_accounts/busy?limit=20&offset=0&performer=${this.query.performer}&search=${this.query.search}`, {
+            credentials: 'include'
+        });
+        const data = await performerResults.json();
+
+        this.performers = data.performerAccounts;
+    }
 }
--- src/router/index.ts
+++ src/router/index.ts
@@ -7,7 +7,7 @@ import Performers from '../components/pages/performers/performers';
 import Favourites from '../components/pages/performers/favourites';
 import Account from '../components/pages/account/account';
 
-import { countryInterceptor } from './localization';
+import { countryInterceptor, authenticatedInterceptor } from './interceptors';
 
 Vue.use(Router);
 
@@ -27,7 +27,8 @@ export default new Router({
                         {
                             path: 'my-account',
                             name: 'Account',
-                            component: Account
+                            component: Account,
+                            beforeEnter: authenticatedInterceptor
                         },
                         {
                             path: 'favourites',
