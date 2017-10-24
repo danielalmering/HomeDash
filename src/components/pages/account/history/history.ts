4a0e91e16eb05f0f914b697ab6783f179f4e60f7
daniel almering <danielalmering@sensemakers.nl>
2017-10-24 16:47:25 +0200

    Added more pages

---
+++ src/components/pages/account/history/history.ts
@@ -0,0 +1,8 @@
+import { Component, Prop } from 'vue-property-decorator';
+import Vue from 'vue';
+
+@Component({
+    template: require('./history.tpl.html')
+})
+export default class History extends Vue {
+}
