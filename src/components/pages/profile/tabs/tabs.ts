3fd0f3c68458081a3b81de14b68331da91b701ab
Lorenzo Vuur <epicokapi@gmail.com>
2017-11-01 15:40:20 +0100

    Tabs split up in different files + made switching working

--- src/components/pages/profile/tabs/tabs.ts
+++ src/components/pages/profile/tabs/tabs.ts
@@ -5,12 +5,29 @@ import './tabs.scss';
 
 @Component({
     template: require('./tabs.tpl.html'),
+    components: {
+        videoChat: { template: require('./videochat.tpl.html') },
+        videoCall: { template: require('./videocall.tpl.html') },
+        call: { template: require('./call.tpl.html') },
+        mail: { template: require('./mail.tpl.html') },
+        sms: { template: require('./sms.tpl.html') }
+    }
 })
 export default class Tabs extends Vue {
 
-    selectedTab = '';
+    selectedTab: string = 'videoChat';
 
-    startSession(){
-        this.$emit('startSession');
+    ivrCode: string = '';
+    displayName: string = '';
+
+    selectTab(newTab: string){
+        this.selectedTab = newTab;
+    }
+
+    startSession(ivrCode: string, displayName: string){
+        this.$emit('startSession', {
+            ivrCode: ivrCode,
+            displayName: displayName
+        });
     }
 }
