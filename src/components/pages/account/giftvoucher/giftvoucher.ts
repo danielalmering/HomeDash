efc41ff3b7fbf0b29c645ddab6dcc11967c045c7
Lorenzo Vuur <epicokapi@gmail.com>
2017-11-02 15:07:59 +0100

    Added gift voucher functionality

--- src/components/pages/account/giftvoucher/giftvoucher.ts
+++ src/components/pages/account/giftvoucher/giftvoucher.ts
@@ -1,9 +1,34 @@
 import { Component, Prop, Watch } from 'vue-property-decorator';
 import Vue from 'vue';
 
+import config from '../../../../config';
+
 @Component({
     template: require('./giftvoucher.tpl.html')
 })
 export default class Giftvoucher extends Vue {
+    voucherCode: string = '';
 
+    async activateVoucher(){
+        const user = this.$store.state.authentication.user;
+        const voucherCode = this.voucherCode.toUpperCase();
+
+        const voucherResult = await fetch(`${config.BaseUrl}/client/client_accounts/${user.id}/promo_credit/${voucherCode}`, {
+            credentials: 'include'
+        });
+
+        if(!voucherResult.ok){
+            this.$store.dispatch('openMessage', {
+                content: 'account.voucher.errorMessage',
+                class: 'error'
+            });
+
+            return;
+        }
+
+        this.$store.dispatch('openMessage', {
+            content: 'account.voucher.successMessage',
+            class: 'success'
+        });
+    }
 }
