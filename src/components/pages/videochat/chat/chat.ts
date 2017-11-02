9dc324ea0487cb6cbb0d75a1de530896a0ec6e2c
Lorenzo Vuur <epicokapi@gmail.com>
2017-11-02 19:49:25 +0100

    Moved chat to seperate component and fixed some issues

--- src/components/pages/videochat/chat/chat.ts
+++ src/components/pages/videochat/chat/chat.ts
@@ -5,12 +5,48 @@ import notificationSocket from '../../../../socket';
 
 import './chat.scss';
 
+interface ChatMessage {
+    senderType: string;
+    message: string;
+}
+
 @Component({
-    template: require('./chat.tpl.html')
+    template: require('./chat.tpl.html'),
+    props: {
+        performerName: {
+            required: true,
+            type: String
+        },
+        userName: {
+            required: true,
+            type: String
+        }
+    }
 })
 export default class Chat extends Vue {
 
+    chatOpened: boolean = true;
+
     chatMessage: string = '';
+    chatMessages: ChatMessage[] = [];
+
+    chatSocketRef: number;
+
+    mounted(){
+        this.chatSocketRef = notificationSocket.subscribe('msg', (content) => {
+            this.chatMessages.push(content);
+
+            const chatContainer = this.$el.querySelector('.videochat__chat-list');
+
+            if(!chatContainer) return;
+
+            this.$nextTick(() => chatContainer.scrollTo(0, chatContainer.scrollHeight));
+        });
+    }
+
+    beforeDestroy(){
+        notificationSocket.unsubscribe(this.chatSocketRef);
+    }
 
     sendMessage(){
         notificationSocket.sendCustomEvent('msg', {
