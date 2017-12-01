0cdbf84c32ab21171a60ebf5cfbb139c21e46323
Karel Brascamp <karel@hobbes.local>
2017-12-01 08:50:44 +0100

    Broadcasting in Flash

--- src/components/pages/videochat/videochat.ts
+++ src/components/pages/videochat/videochat.ts
@@ -7,6 +7,7 @@ import { SessionData } from '../../../store/Session';
 
 import notificationSocket from '../../../socket';
 import Chat from './chat/chat';
+import Broadcast, { Caster } from './broadcast/broadcast';
 import config from '../../../config';
 
 import './videochat.scss';
@@ -14,7 +15,8 @@ import './videochat.scss';
 @Component({
     template: require('./videochat.tpl.html'),
     components: {
-        chat: Chat
+        chat: Chat,
+        broadcast: Broadcast
     }
 })
 export default class VideoChat extends Vue {
@@ -23,9 +25,30 @@ export default class VideoChat extends Vue {
     isEnding: boolean = false;
     chatMessages: any[] = [];
 
-    player: any;
+    player: jsmpeg.Player;
     intervalTimer: number;
     chatSocketRef: number;
+
+    broadcasting: {cam:boolean | string, mic:boolean | string, settings:boolean} = {cam:false, mic:false, settings:false};
+    stateMessages:string[] = [];
+
+
+    cameras:{name:string, selected:boolean}[];
+    microphones:{name:string, selected:boolean}[];
+
+    get wowza():string | undefined{
+        if (!this.$store.state.session.activeSessionData){
+            return undefined;
+        }
+        return this.$store.state.session.activeSessionData.wowza;
+    }
+
+    get streamName():string | undefined{
+        if (!this.$store.state.session.activeSessionData){
+            return undefined;
+        }
+        return this.$store.state.session.activeSessionData.publishStream;
+    }
 
     get performer(){
         return this.$store.state.session.activePerformer;
@@ -87,6 +110,73 @@ export default class VideoChat extends Vue {
 
     close(){
         this.$router.push({ name: 'Profile', params: { id: this.$route.params.id } });
+    }
+
+    startCam(){
+        this.broadcasting.cam = true;
+    }
+
+    toggleCam(){
+        this.broadcasting.cam = !this.broadcasting.cam;
+        //also hide the settings screen when the cam is turned off
+        if (!this.broadcasting.cam){
+            this.broadcasting.settings = false;
+        }
+    }
+
+    toggleMic(){
+        this.broadcasting.mic = !this.broadcasting.mic;
+        //replace the boolean with the actual name if the selected mic is showing..
+        if (this.broadcasting.settings && this.broadcasting.mic){
+            const flash:Caster = this.$el.querySelector('#broadcastSWF') as any;
+            this.microphones = flash.getMicrophones();
+            console.log(this.microphones);
+            const selected = this.microphones.find( mic=>mic.selected);
+            if (selected){
+                this.broadcasting.mic = selected.name;
+            }
+        }
+    }
+
+    setCamera(event:Event){
+        const camName = (<HTMLSelectElement>event.srcElement).value;
+        this.cameras.forEach( cam=> cam.selected = (cam.name==camName) );
+        this.broadcasting.cam = camName;
+    }
+
+    setMicrophone(event:Event){
+        const micName = (<HTMLSelectElement>event.srcElement).value;
+        this.microphones.forEach( mic=>mic.selected = (mic.name == micName) );
+        this.broadcasting.mic = micName;
+    }
+
+    broadcastStateChange(state:string){
+        this.stateMessages.push(state);
+    }
+
+    broadcastError(message:string){
+        this.stateMessages.push(message);
+    }
+ 
+    toggleSettings(){
+        this.broadcasting.settings = !this.broadcasting.settings;
+        //go get the list of devices if the "settings" will toggle to visible
+        if (this.broadcasting.settings){
+            const flash:Caster = this.$el.querySelector('#broadcastSWF') as any;
+
+            this.cameras = flash.getCameras();
+            let selected = this.cameras.find( cam=>cam.selected );
+            if (selected && this.broadcasting.cam != selected.name){
+                this.broadcasting.cam = selected.name;
+            }
+
+            this.microphones = flash.getMicrophones();
+            console.log(this.microphones);
+            selected = this.microphones.find( mic=>mic.selected);
+            if (selected && this.broadcasting.mic && this.broadcasting.mic != selected.name){
+                this.broadcasting.mic = selected.name;
+            }
+        }   
     }
 
     beforeDestroy(){
