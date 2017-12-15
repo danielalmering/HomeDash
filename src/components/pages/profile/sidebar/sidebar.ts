75849766c54a915fd30f2d8b2ca5790fee577176
Karel Brascamp <karel@hobbes.home>
2017-12-15 09:25:14 +0100

    Peek change

--- src/components/pages/profile/sidebar/sidebar.ts
+++ src/components/pages/profile/sidebar/sidebar.ts
@@ -8,6 +8,7 @@ import config from '../../../../config';
 
 import './sidebar.scss';
 import JSMpeg from '../../videochat/streams/jsmpeg';
+import { SessionType } from '../../../../models/Sessions';
 
 type SidebarCategory = 'recommended' | 'peek' | 'favourites' | 'voyeur';
 
@@ -118,6 +119,18 @@ export default class Sidebar extends Vue {
     }
 
     goToPerformer(id: number){
+        const session = this.$store.state.session;
+        //peek with another lady if you're currently peeking and the lady is peekable
+        if (this.category == 'peek' && session.activeSessionType == SessionType.Peek){
+            this.$router.push({
+                name: 'Videochat',
+                params: {
+                    id: id.toString()
+                }
+            });
+            return;
+        }
+
         this.$router.push({
             name: 'Profile',
             params: {
