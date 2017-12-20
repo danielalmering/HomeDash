b7d79149bf4c014c688568193ea2899a76066185
Hotze Visser <hotzevisser@Hotzes-MacBook-Pro.local>
2017-12-20 11:33:38 +0100

    nanocosmos player integration initial

--- src/config.ts
+++ src/config.ts
@@ -6,6 +6,8 @@ export interface ProjectConfig {
     JsmpegUrl: string;
     StorageKey: string;
     NoAgeCheckCountries: string[];
+    H5Server: string;
+    H5FlashSwf: string;
 }
 
 export default require(`./private.${process.env.NODE_ENV}.json`) as ProjectConfig;
