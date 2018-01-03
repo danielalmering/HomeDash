30d004baf2e25c4e90c81a841ab091fde558c432
Lorenzo Vuur <epicokapi@gmail.com>
2018-01-03 16:30:41 +0100

    Close menus when clicking body

---
+++ src/directives/clickOutside.ts
@@ -0,0 +1,27 @@
+import Vue from 'vue';
+import { VNode, VNodeDirective } from 'vue/types/vnode';
+
+Vue.directive('clickOutside', {
+    inserted: function(el: HTMLElement, binding: VNodeDirective){
+        const callback = binding.value.cb as () => void;
+        const anyEl = el as any;
+
+        anyEl.onOutsideClick = function(event: Event) {
+            const target = event.target as HTMLElement;
+
+            const ignored = binding.value.ignoreClass as string[];
+            const hasIgnored = ignored.some(i => target.classList.contains(i));
+
+            if(hasIgnored){
+                return;
+            }
+
+            callback();
+        };
+
+        document.addEventListener('click', anyEl.onOutsideClick);
+    },
+    unbind: function(el: HTMLElement){
+        document.removeEventListener('click', (<any>el).onOutsideClick);
+    }
+});
