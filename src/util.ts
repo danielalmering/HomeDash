978270472f9313a0f4a964a6f8019963107a3640
daniel almering <danielalmering@sensemakers.nl>
2018-02-15 15:39:47 +0100

    Added logic to selected tabs with karel

--- src/util.ts
+++ src/util.ts
@@ -67,6 +67,42 @@ export function getPerformerLabel(performer: Performer){
     return 'none';
 }
 
+export function serviceEnabled(service: string, forPerformer: Performer):boolean { 
+    if (!forPerformer){
+        return false;
+    }
+
+    //services:
+    //cam,email,peek,phone,sms,videocall,voicemail
+    //voyeur is an exception..
+    if (service === 'voyeur'){
+        return forPerformer.isVoyeur;
+    }
+
+    if (!(service in forPerformer.performer_services) ){
+        throw new Error(`${service} ain't no service I ever heard of!`);
+    }
+
+    // Email & sms enabled independently of status from performer!
+    if(service === 'email' || service === 'sms' ){
+        return forPerformer.performer_services[service];
+    }
+
+    // If performer is calling no service enabled
+    if(forPerformer.performerStatus === PerformerStatus.OnCall){
+        return false;
+    }
+
+    // If performer is busy and cam or peek are enabled!
+    if(forPerformer.performerStatus === PerformerStatus.Busy){
+        return service === 'cam' && forPerformer.performer_services['peek'];
+    }
+
+    // The default scenario of status from services
+    return forPerformer.performer_services[service];
+
+}
+
 export function openModal(name: string){
     this.$store.dispatch('displayModal', name);
 }
