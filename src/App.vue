46809f0b0e045cc6451f4b584b0c297cc3bfcc14
Hotze Visser <hvisser@senk.local>
2019-10-16 13:58:33 +0200

    Test with storageFactory

--- src/App.vue
+++ src/App.vue
@@ -1,125 +1,167 @@
 <template>
-    <div id="app">
-        <modal-wrapper></modal-wrapper>
-        <cookies v-if="displayCookies" v-on:close="displayCookies = false"></cookies>
-        <router-view/>
-        <agecheck v-if="displayAgecheck" v-on:close="displayAgecheck = false"></agecheck>
-        <alerts></alerts>
-    </div>
+  <div id="app">
+    <modal-wrapper></modal-wrapper>
+    <cookies v-if="displayCookies" v-on:close="displayCookies = false"></cookies>
+    <router-view />
+    <agecheck v-if="displayAgecheck" v-on:close="displayAgecheck = false"></agecheck>
+    <alerts></alerts>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import { Component } from 'vue-property-decorator';
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
 
-import modalWrapper from './components/modal/modal-wrapper';
-import notificationSocket from './socket';
-import { SocketMessageEventArgs } from './models/Socket';
-import { getParameterByName } from './util';
+import modalWrapper from "./components/modal/modal-wrapper";
+import notificationSocket from "./socket";
+import { SocketMessageEventArgs } from "./models/Socket";
+import { getParameterByName, storageFactory } from "./util";
 
-import alerts from './components/layout/Alerts.vue';
-import cookies from './components/layout/Cookies.vue';
-import agecheck from './components/layout/Agecheck.vue';
+import alerts from "./components/layout/Alerts.vue";
+import cookies from "./components/layout/Cookies.vue";
+import agecheck from "./components/layout/Agecheck.vue";
 
-import config from './config';
-import * as Sentry from '@sentry/browser'
-import 'whatwg-fetch';
+import config from "./config";
+import * as Sentry from "@sentry/browser";
+import "whatwg-fetch";
 
 @Component({
-    components: {
-        modalWrapper: modalWrapper,
-        alerts: alerts,
-        cookies: cookies,
-        agecheck: agecheck
-    }
+  components: {
+    modalWrapper: modalWrapper,
+    alerts: alerts,
+    cookies: cookies,
+    agecheck: agecheck
+  }
 })
 export default class Cookies extends Vue {
-    localStorage: boolean = false;
-    displayCookies: boolean = false;
-    displayAgecheck: boolean = false;
-    getParameterByName = getParameterByName;
+  localStorage: boolean = false;
+  displayCookies: boolean = false;
+  displayAgecheck: boolean = false;
+  getParameterByName = getParameterByName;
 
-    mounted(){
-        // SafeMode
-        const safeMode = this.getParameterByName('safe');
-        const gotsafe = safeMode ? this.$store.commit('activateSafeMode') : this.$store.commit('deactivateSafeMode');
+  localStore = storageFactory(window.localStorage);
+  sessionStore = storageFactory(window.sessionStorage);
+
+
+
+  mounted() {
+    // SafeMode
+    const safeMode = this.getParameterByName("safe");
+    const gotsafe = safeMode
+      ? this.$store.commit("activateSafeMode")
+      : this.$store.commit("deactivateSafeMode");
+  }
+
+  async created() {
+    const utmMedium = this.getParameterByName("utm_medium");
+
+    await this.$store.dispatch("getSession", false);
+
+    if (!utmMedium || utmMedium.toLowerCase() !== "advertising") {
+      this.$store.dispatch("intervalChecksession");
+      await this.$store.dispatch("setLanguage", config.locale.DefaultLanguage);
     }
 
-    async created(){
-        const utmMedium = this.getParameterByName('utm_medium');
+    notificationSocket.subscribe("message", (data: SocketMessageEventArgs) => {
+      this.$store.dispatch("successMessage", "general.successNewMessage");
+    });
 
-        await this.$store.dispatch('getSession', false);
+    // Geo Safe check
+    const geoResult = await fetch(`${config.BaseUrl}/loc`, {
+      credentials: "include"
+    });
+    const geoLocations = ["DE", "BE", "NL", "LU"];
+    const result = await geoResult.json();
 
-        if(!utmMedium || utmMedium.toLowerCase() !== 'advertising'){
+    if (geoResult.ok && geoLocations.indexOf(result.country_code) !== -1) {
+      this.$store.commit("deactivateSafeMode");
+    }
 
-            this.$store.dispatch('intervalChecksession');
-            await this.$store.dispatch('setLanguage', config.locale.DefaultLanguage);
-        }
+    /*try {
+      // Localstorage check
+      window.localStorage.setItem(`${config.StorageKey}.localStorage`, "true");
+      window.localStorage.removeItem(`${config.StorageKey}.localStorage`);
 
-        notificationSocket.subscribe('message', (data: SocketMessageEventArgs) => {
-            this.$store.dispatch('successMessage', 'general.successNewMessage');
+  
+
+      // Cookies check
+      const cookiesAccepted =
+        window.localStorage.getItem(`${config.StorageKey}.cookiesAccepted`) !==
+        null
+          ? window.localStorage.getItem(`${config.StorageKey}.cookiesAccepted`)
+          : false;
+      this.displayCookies = !(cookiesAccepted && cookiesAccepted === "true");
+
+      // Agecheck check
+      const AgeCheckAccepted =
+        window.localStorage.getItem(`${config.StorageKey}.agecheck`) !== null
+          ? window.localStorage.getItem(`${config.StorageKey}.agecheck`)
+          : false;
+      this.displayAgecheck = !config.locale.AgeCheck
+        ? false
+        : !(AgeCheckAccepted && AgeCheckAccepted === "true");
+    } catch (error) {
+      if (
+        error.name === "QuotaExceededError" ||
+        error.name === "SecurityError"
+      ) {
+        // Switch to sessionStore when IOS for now
+        Object.assign(window.localStorage, window.sessionStorage);
+        this.displayCookies = true;
+        this.displayAgecheck = config.locale.AgeCheck;
+      } else {
+        this.$store.dispatch("errorMessage", "general.errorLocalstorage");
+        window.localStorage.clear();
+        window.location.reload();
+      }
+    }*/
+
+    const cookiesAccepted =
+        this.localStore.getItem(`${config.StorageKey}.cookiesAccepted`) !==
+        null
+          ? this.localStore.getItem(`${config.StorageKey}.cookiesAccepted`)
+          : false;
+      this.displayCookies = !(cookiesAccepted && cookiesAccepted === "true");
+
+      // Agecheck check
+      const AgeCheckAccepted =
+        this.localStore.getItem(`${config.StorageKey}.agecheck`) !== null
+          ? this.localStore.getItem(`${config.StorageKey}.agecheck`)
+          : false;
+      this.displayAgecheck = !config.locale.AgeCheck
+        ? false
+        : !(AgeCheckAccepted && AgeCheckAccepted === "true");
+
+    let registrationAttempts = 0;
+
+    //  Hotjar
+    const HotjarAccepted = config.locale.Hotjar ? registerHotjarToSentry() : "";
+
+    function registerHotjarToSentry() {
+      const hj = window.hj as any;
+      registrationAttempts += 1;
+
+      if (
+        hj &&
+        hj.pageVisit &&
+        hj.pageVisit.property &&
+        hj.pageVisit.property.key
+      ) {
+        const hotjarUserId = hj.pageVisit.property.key;
+
+        Sentry.addBreadcrumb({
+          message: `Sentry session started with hotjar user ${hotjarUserId}`,
+          category: "data"
         });
-
-        // Geo Safe check
-        const geoResult = await fetch(`${config.BaseUrl}/loc`, { credentials: 'include'});
-        const geoLocations = ['DE', 'BE', 'NL', 'LU'];
-        const result = await geoResult.json();
-
-        if(geoResult.ok && geoLocations.indexOf(result.country_code) !== -1){
-            this.$store.commit('deactivateSafeMode');
-        }
-
-        try {
-            // Localstorage check
-            window.localStorage.setItem(`${config.StorageKey}.localStorage`, 'true');
-            window.localStorage.removeItem(`${config.StorageKey}.localStorage`);
-
-            // Cookies check
-            const cookiesAccepted = (window.localStorage.getItem(`${config.StorageKey}.cookiesAccepted`) !== null ) ? window.localStorage.getItem(`${config.StorageKey}.cookiesAccepted`) : false;
-            this.displayCookies = !(cookiesAccepted && cookiesAccepted === 'true');
-
-            // Agecheck check
-            const AgeCheckAccepted = (window.localStorage.getItem(`${config.StorageKey}.agecheck`) !== null ) ? window.localStorage.getItem(`${config.StorageKey}.agecheck`) : false;
-            this.displayAgecheck = !config.locale.AgeCheck ? false : !(AgeCheckAccepted && AgeCheckAccepted === 'true');
-
-        } catch(error){
-            if(error.name === 'QuotaExceededError' || error.name === 'SecurityError'){
-                // Switch to sessionStore when IOS for now
-                Object.assign(window.localStorage, window.sessionStorage);                
-                this.displayCookies = true;
-                this.displayAgecheck = config.locale.AgeCheck;
-            } else {
-                this.$store.dispatch('errorMessage', 'general.errorLocalstorage');
-                window.localStorage.clear();
-                window.location.reload();
-            }
-        }
-
-        let registrationAttempts = 0;
-
-        //  Hotjar
-        const HotjarAccepted = config.locale.Hotjar ? registerHotjarToSentry() : '';
-
-        function registerHotjarToSentry(){
-            const hj = window.hj as any;
-            registrationAttempts += 1;
-
-            if(hj && hj.pageVisit && hj.pageVisit.property && hj.pageVisit.property.key){
-                const hotjarUserId = hj.pageVisit.property.key;
-
-                Sentry.addBreadcrumb({
-                    message: `Sentry session started with hotjar user ${hotjarUserId}`,
-                    category: 'data'
-                });
-            } else if(registrationAttempts <= 5) {
-                setTimeout(registerHotjarToSentry, 2000);
-            } else {
-                Sentry.addBreadcrumb({
-                    message: `Could not register hotjar`,
-                    category: 'data'
-                });
-            }
-        }
+      } else if (registrationAttempts <= 5) {
+        setTimeout(registerHotjarToSentry, 2000);
+      } else {
+        Sentry.addBreadcrumb({
+          message: `Could not register hotjar`,
+          category: "data"
+        });
+      }
     }
+  }
 }
 </script>
